aa9e1165396c5c71965d1b6cc3cc07b48aecd006
Vertrak <56128327+Vertrak@users.noreply.github.com>
2019-12-23 15:40:30 -0500

    updated connectSerial and getVoltage
    ■connectSerial now loops until device name verified
    ■getVoltage renamed to getBatStatus
    ■getBatStatus finished

--- ArduinoFunctions.py
+++ ArduinoFunctions.py
@@ -1,104 +1,112 @@
-from serial import *                                #import serial library functions
-from sys import exit                                #import exit from sys library
+from serial import *								#import serial library functions
+from sys import exit								#import exit from sys library
 
-#determine 
-def getVoltage(an):
-    if an > 925:
-        status = "Battery charging"
-    elif an > 700: 
-                return status
+#determine state of battery
+def getBatStatus(an):
+	status = 0
+	if an > 925:
+		status = "Overcharged"
+	elif an > 872
+		status = "100%"
+	elif an > 833:
+		status = "80%"
+	elif an > 807:
+		status = "50%"
+	elif an > 763:
+		status = "20%"
+	else:
+		status = "Dead"
+	return status
 
-#determine 
+#determine whether it's sunny or cloudy
 def getLight(an):
-    return 
+	return (an > )
 
 #multiply ticks by rotation factor in MPH
 def getWindSpeed(ticks):
-    return ticks * 1.492
+	return ticks * 1.492
 
 #multiple ticks by volume factor in inches
 def getRainVolume(ticks):
-    return ticks * .011
+	return ticks * .011
 
 def getWindDirection(an):
-    #python has no switch. Dictionary acts as switch statement
-    #create dictionary with calibrated direction values
-    directions = {
-        788:    "N",
-        407:    "NNE",
-        462:    "NE",
-        82:     "ENE",
-        91:     "E",
-        64:     "ESE",
-        183:    "SE",
-        125:    "SSE",
-        287:    "S",
-        244:    "SSW",
-        631:    "SW",
-        600:    "WSW",
-        945:    "W",
-        828:    "WNW",
-        887:    "NW",
-        704:    "NNW"
-    }
-    return directions.get(an)   #get direction
+	#python has no switch. Dictionary acts as switch statement
+	#create dictionary with calibrated direction values
+	directions = {
+		788:	"N",
+		407:	"NNE",
+		462:	"NE",
+		82:		"ENE",
+		91:		"E",
+		64:		"ESE",
+		183:	"SE",
+		125:	"SSE",
+		287:	"S",
+		244:	"SSW",
+		631:	"SW",
+		600:	"WSW",
+		945:	"W",
+		828:	"WNW",
+		887:	"NW",
+		704:	"NNW"
+	}
+	return directions.get(an)	#get direction
 
 #does comparisons for snow estimation
 def getSnow(an, temp, hum):
-    if an > 680 and temp < 0:                                   #sensor and temp comp
-        reading = "freezing, possible sleet"                    #corresponding message
-    elif an > 680 and temp < 13:                                #sensor and temp comp
-        reading = "dry and cold"                                #corresponding message
-    elif an < 680 and ((temp < 0 and hum < 30) or temp < -6):   #sensor and temp comp
-        reading = "possible snow"                               #corresponding message
-    else:                                                       #otherwise
-        reading = "no snow, not cold"                           #corresponding message
-    return reading                                              #return message
+	if an > 680 and temp < 0:									#sensor and temp comp
+		reading = "freezing, possible sleet"					#corresponding message
+	elif an > 680 and temp < 13:								#sensor and temp comp
+		reading = "dry and cold"								#corresponding message
+	elif an < 680 and ((temp < 0 and hum < 30) or temp < -6):	#sensor and temp comp
+		reading = "possible snow"								#corresponding message
+	else:														#otherwise
+		reading = "no snow, not cold"							#corresponding message
+	return reading												#return message
 
 #read a set of values from the arduino
 def readSerial(connection, name):
-    if name == "Moteino":                               #for Moteino connection
-        values = []                                     #create an empty list
-        for i in range(0,7):                    #loop n-1 times for most data
-            values.append(connection.readline())        #read data from the connection
-            values[i] = values[i].decode()              #translate bytes to string
-            values[i] = int(values[i])                  #convert to int
-        values.append(connection.readline())            #read data from the connection
-        values[i] = values[i].decode()                  #translate bytes to string
-        values[i] = float(values[LAST])                 #convert to float (battery Voltage)
-    elif name == "CC1101":                              #for CC1101 connection
-        values = connection.readline()                  #read a byte from the connection
-    else:                                               #all other cases
-        values = None                                   #set to null
-    return values                                       #return values
+	if name == "Moteino":								#for Moteino connection
+		values = []										#create an empty list
+		for i in range(5):					#loop n-1 times for most data
+			values.append(connection.readline())		#read data from the connection
+			values[i] = values[i].decode()				#translate bytes to string
+			values[i] = int(values[i])					#convert to int
+		values.append(connection.readline())			#read data from the connection
+		values[i] = values[i].decode()					#translate bytes to string
+		values[i] = float(values[LAST])					#convert to float (battery Voltage)
+	elif name == "CC1101":								#for CC1101 connection
+		values = connection.readline()					#read a byte from the connection
+	else:												#all other cases
+		values = None									#set to null
+	return values										#return values
 
 #read a line from the serial connection and convert it from bytes
 def connectSerial():
-
-    port = "/dev/ttyUSB{}" #windows: "com{}", linux:    #create port string
-    i = 0                                               #create a counter
-    while True:                                         #loop forever
-        try:                                            #try the following code
-            connection = Serial(port.format(i), 9600)   #open serial connection at 9600 baud
-            print("connected to serial")
-            break                                       #break out of the while loop
-        except SerialException:                         #catch exception
-            i += 1                                      #increment counter
-            if i > 256:                                 #check for com port limit
-                return (None, None)                             #no port opened
-    name = connection.readline()                        #read data from the connection
-    print(name)
-    name = name.decode()                                #decode the message
-    print(name)
-    if name == "Moteino\r\n":                #execute on "moteino"
-        name = "Moteino"                                #set to simpler string
-        print("got moteino")
-    elif name == "CC1101\r\n":                           #execute on "cc1101" 
-        name = "CC1101"                                 #set to simpler string
-        print("got cc1101")
-    else:                                               #execute for other cases
-        name = None                                     #no device name given
-        print("got nottin")
-    return (connection, name)                           #return connection and device name
-    
-
+	port = "com{}"	#windows: "com{}", linux: "/dev/ttyUSB{}"	#create port string
+	i = 0												#create a counter
+	while True:											#loop forever
+		try:											#try the following code
+			connection = Serial(port.format(i), 9600)	#open serial connection at 9600 baud
+			break										#break out of the while loop
+		except SerialException:							#catch exception
+			i += 1										#increment counter
+			if i > 256:									#check for com port limit
+				return (None, None)						#no port opened
+	while True:											#loop forever
+		data = connection.readline()					#read data from the connection
+		try:											#try the following code
+			data = data.decode()						#decode the message
+		except UnicodeDecodeError:						#catch decoding error
+			continue									#go to beginning of loop
+		print(data)										#print the message
+		if (data) == "Moteino\r\n":						#execute on "moteino"
+			name = "Moteino"							#set to simpler string
+			break										#exit loop
+		elif name == "CC1101\r\n":						#execute on "cc1101" 
+			name = "CC1101"								#set to simpler string
+			break										#exit loop
+	while connection.readline() != "Good!\r\n":			#wait for 
+		connect.write("■")								#send 0xFE
+	return (connection, name)							#return port and device name
