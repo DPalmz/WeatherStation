4503a324006b15180a67adfed9069d9b902ff4e4
Pitato <pi@raaspberrypi>
2020-01-15 21:58:52 -0500

    last change of the day

--- cc1101USB.py
+++ cc1101USB.py
@@ -3,7 +3,7 @@ ser = serial.Serial("/dev/ttyUSB0",9600)
 ser.flushInput()
 
 while True:
-    if(ser.in_waiting >0):
+   # if(ser.in_waiting >0):
         data = ser.readline()
         print(data)
 
